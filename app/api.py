52077e63bbd50a1f48233bab260f077fd4aeb94e
PeadarOhAodha <ucabpnh@ucl.ac.uk>
2020-04-11 21:37:51 +0100

    init commit for question generator service

--- app/api.py
+++ app/api.py
@@ -1,1 +1,36 @@
 """ Our api for exposing our models"""
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import HTMLResponse
+import uvicorn
+from models.question_gen import QuestionGenerator
+
+
+# api setup
+class RequestData(BaseModel):
+    text_data: List[str]
+
+app = FastAPI(title="Memorai APP",
+              description="AI memory assistant",
+              version="0.0.1")
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=["*"],
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"])
+
+# app endpoints
+@app.get("/")
+def root():
+    return HTMLResponse("""
+    <h1>Memorai API</h1>
+    <p>See <a href='/docs'>docs</a></p>""")
+
+
+@app.post('question/')
+def gen_question(text:RequestData):
+    return 0
--- models/question_gen.py
+++ models/question_gen.py
@@ -1,1 +1,13 @@
 """ Our state of the art question generator AI """
+
+
+class QuestionGenerator:
+
+    def __init__(self) -> None:
+        pass
+
+    def gen_question(self, text: str) -> str:
+        return 'question'
+
+    def get_embeddings(self):
+        return 0
