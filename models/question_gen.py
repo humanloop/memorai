5816fe16f5c1af3d7d1e820ec0fae9ff4e303e87
PeadarOhAodha <ucabpnh@ucl.ac.uk>
2020-04-12 00:06:26 +0100

    some cleanup

--- models/question_gen.py
+++ models/question_gen.py
@@ -28,22 +28,16 @@ def pickleExists(fileName):
 # Extract answers and the sentence they are in
 def extractAnswers(qas, doc):
     answers = []
-
     senStart = 0
     senId = 0
-
     for sentence in doc.sents:
         senLen = len(sentence.text)
-
         for answer in qas:
             answerStart = answer['answers'][0]['answer_start']
-
             if (answerStart >= senStart and answerStart < (senStart + senLen)):
                 answers.append({'sentenceId': senId, 'text': answer['answers'][0]['text']})
-
         senStart += senLen
         senId += 1
-
     return answers
 
 
@@ -61,15 +55,12 @@ def getNEStartIndexs(doc):
     neStarts = {}
     for ne in doc.ents:
         neStarts[ne.start] = ne
-
     return neStarts
 
 def getSentenceStartIndexes(doc):
     senStarts = []
-
     for sentence in doc.sents:
         senStarts.append(sentence[0].i)
-
     return senStarts
 
 
@@ -81,32 +72,40 @@ def getSentenceForWordPosition(wordPos, senStarts):
 
 def oneHotEncodeColumns(df):
     columnsToEncode = ['NER', 'POS', "TAG", 'DEP']
-
     for column in columnsToEncode:
         one_hot = pd.get_dummies(df[column])
         one_hot = one_hot.add_prefix(column + '_')
-
         df = df.drop(column, axis = 1)
         df = df.join(one_hot)
-
     return df
 
 
 def prepareDf(df):
     #One-hot encoding
     wordsDf = oneHotEncodeColumns(df)
-
     #Drop unused columns
     columnsToDrop = ['text', 'titleId', 'paragrapghId', 'sentenceId', 'shape']
     wordsDf = wordsDf.drop(columnsToDrop, axis = 1)
-
     #Add missing colums
-    predictorColumns = ['wordCount','NER_CARDINAL','NER_DATE','NER_EVENT','NER_FAC','NER_GPE','NER_LANGUAGE','NER_LAW','NER_LOC','NER_MONEY','NER_NORP','NER_ORDINAL','NER_ORG','NER_PERCENT','NER_PERSON','NER_PRODUCT','NER_QUANTITY','NER_TIME','NER_WORK_OF_ART','POS_ADJ','POS_ADP','POS_ADV','POS_CCONJ','POS_DET','POS_INTJ','POS_NOUN','POS_NUM','POS_PART','POS_PRON','POS_PROPN','POS_PUNCT','POS_SYM','POS_VERB','POS_X','TAG_''','TAG_-LRB-','TAG_.','TAG_ADD','TAG_AFX','TAG_CC','TAG_CD','TAG_DT','TAG_EX','TAG_FW','TAG_IN','TAG_JJ','TAG_JJR','TAG_JJS','TAG_LS','TAG_MD','TAG_NFP','TAG_NN','TAG_NNP','TAG_NNPS','TAG_NNS','TAG_PDT','TAG_POS','TAG_PRP','TAG_PRP$','TAG_RB','TAG_RBR','TAG_RBS','TAG_RP','TAG_SYM','TAG_TO','TAG_UH','TAG_VB','TAG_VBD','TAG_VBG','TAG_VBN','TAG_VBP','TAG_VBZ','TAG_WDT','TAG_WP','TAG_WRB','TAG_XX','DEP_ROOT','DEP_acl','DEP_acomp','DEP_advcl','DEP_advmod','DEP_agent','DEP_amod','DEP_appos','DEP_attr','DEP_aux','DEP_auxpass','DEP_case','DEP_cc','DEP_ccomp','DEP_compound','DEP_conj','DEP_csubj','DEP_csubjpass','DEP_dative','DEP_dep','DEP_det','DEP_dobj','DEP_expl','DEP_intj','DEP_mark','DEP_meta','DEP_neg','DEP_nmod','DEP_npadvmod','DEP_nsubj','DEP_nsubjpass','DEP_nummod','DEP_oprd','DEP_parataxis','DEP_pcomp','DEP_pobj','DEP_poss','DEP_preconj','DEP_predet','DEP_prep','DEP_prt','DEP_punct','DEP_quantmod','DEP_relcl','DEP_xcomp']
-
+    predictorColumns = ['wordCount','NER_CARDINAL','NER_DATE','NER_EVENT','NER_FAC','NER_GPE','NER_LANGUAGE',
+                        'NER_LAW','NER_LOC','NER_MONEY','NER_NORP','NER_ORDINAL','NER_ORG','NER_PERCENT',
+                        'NER_PERSON','NER_PRODUCT','NER_QUANTITY','NER_TIME','NER_WORK_OF_ART','POS_ADJ',
+                        'POS_ADP','POS_ADV','POS_CCONJ','POS_DET','POS_INTJ','POS_NOUN','POS_NUM','POS_PART',
+                        'POS_PRON','POS_PROPN','POS_PUNCT','POS_SYM','POS_VERB','POS_X','TAG_''','TAG_-LRB-','TAG_.',
+                        'TAG_ADD','TAG_AFX','TAG_CC','TAG_CD','TAG_DT','TAG_EX','TAG_FW','TAG_IN','TAG_JJ','TAG_JJR',
+                        'TAG_JJS','TAG_LS','TAG_MD','TAG_NFP','TAG_NN','TAG_NNP','TAG_NNPS','TAG_NNS','TAG_PDT',
+                        'TAG_POS','TAG_PRP','TAG_PRP$','TAG_RB','TAG_RBR','TAG_RBS','TAG_RP','TAG_SYM','TAG_TO',
+                        'TAG_UH','TAG_VB','TAG_VBD','TAG_VBG','TAG_VBN','TAG_VBP','TAG_VBZ','TAG_WDT','TAG_WP',
+                        'TAG_WRB','TAG_XX','DEP_ROOT','DEP_acl','DEP_acomp','DEP_advcl','DEP_advmod','DEP_agent',
+                        'DEP_amod','DEP_appos','DEP_attr','DEP_aux','DEP_auxpass','DEP_case','DEP_cc','DEP_ccomp',
+                        'DEP_compound','DEP_conj','DEP_csubj','DEP_csubjpass','DEP_dative','DEP_dep','DEP_det',
+                        'DEP_dobj','DEP_expl','DEP_intj','DEP_mark','DEP_meta','DEP_neg','DEP_nmod','DEP_npadvmod',
+                        'DEP_nsubj','DEP_nsubjpass','DEP_nummod','DEP_oprd','DEP_parataxis','DEP_pcomp','DEP_pobj',
+                        'DEP_poss','DEP_preconj','DEP_predet','DEP_prep','DEP_prt','DEP_punct','DEP_quantmod',
+                        'DEP_relcl','DEP_xcomp']
     for feature in predictorColumns:
         if feature not in wordsDf.columns:
             wordsDf[feature] = 0
-
     return wordsDf
 
 
@@ -115,15 +114,12 @@ def blankAnswer(firstTokenIndex, lastTokenIndex, sentStart, sentEnd, doc):
     leftPartEnd = doc[firstTokenIndex].idx
     rightPartStart = doc[lastTokenIndex].idx + len(doc[lastTokenIndex])
     rightPartEnd = doc[sentEnd - 1].idx + len(doc[sentEnd - 1])
-
     question = doc.text[leftPartStart:leftPartEnd] + '_____' + doc.text[rightPartStart:rightPartEnd]
-
     return question
 
 
 def sortAnswers(qaPairs):
     orderedQaPairs = sorted(qaPairs, key=lambda qaPair: qaPair['prob'])
-
     return orderedQaPairs
 
 
@@ -141,7 +137,6 @@ class QuestionGenerator:
         return df
 
     def gen_question(self, q_type: str, text: str, num_qs=4) -> list:
-
         if q_type == 'cloze':
             # Extract words
             df = self.generate_df(text)
