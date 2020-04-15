f4202a86cb89f080b63e0ab96b92b097b83e0ea9
Jordan Burgess <jordanburgess@gmail.com>
2020-04-15 15:46:07 +0100

    Update fastapi and use inbuilt CORS
    this wasn't the cause but we may as well

--- app/api.py
+++ app/api.py
@@ -1,8 +1,8 @@
 """ Our api for exposing our models"""
 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse
 import uvicorn
 from models.question_gen import QuestionGenerator
@@ -17,16 +17,21 @@ app = FastAPI(title="Memorai APP",
               description="AI memory assistant",
               version="0.0.1")
 
-origins = ["http://3.22.209.159", "http://questions.humanloop.ml", "https://questions.humanloop.ml"]
-
-app.add_middleware(CORSMiddleware,
-                   allow_origins=origins,
-                   allow_credentials=True,
-                   allow_methods=["DELETE", "GET", "POST", "PUT", "OPTIONS"],
-                   allow_headers=["*"])
+origins = [
+    "http://localhost",
+    "http://3.22.209.159",
+    "http://questions.humanloop.ml",
+    "https://questions.humanloop.ml"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 q_gen = QuestionGenerator()
-
 
 # app endpoints
 @app.get("/")
@@ -42,6 +47,7 @@ def gen_question(text: RequestData, q_type: str = 'cloze'):
         return q_gen.gen_question(q_type, text.dict()['text_data'])
     except Exception:
         return HTTPException(500, {'debug_info': 'series type not supported'})
+
 
 @app.options('/question/')
 def gen_question():
